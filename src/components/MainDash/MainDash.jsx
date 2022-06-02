d0fd1e2dbf9c3731bd5a6e26e16253c98c9b4be2
Laura THAY <laura.thay@ipag.fr>
2022-06-02 15:07:40 +0200

    installed Table

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Cards from '../Cards/Cards'
+import Table from '../Table/Table'
 import './MainDash.css'
 
 const MainDash = () => {
@@ -8,6 +9,7 @@ const MainDash = () => {
     <div className="MainDash">
         <h1> Dashboard </h1>
         <Cards />
+        <Table />
     </div>
   )
 
